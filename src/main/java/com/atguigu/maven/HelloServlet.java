9b298855ff5eca424e79e6813d0f891d7a57cb65
fenghao <chinafenghao@hotmail.com>
2023-10-21 14:26:49 +0800

    引入compile类型的依赖

--- src/main/java/com/atguigu/maven/HelloServlet.java
+++ src/main/java/com/atguigu/maven/HelloServlet.java
@@ -5,11 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.math.BigInteger;
+import com.atguigu.maven.Calculate;
 
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
         try {
-            resp.getWriter().write("hello maven web project");
+            Calculate calculate = new Calculate();
+            int addRes = calculate.add(5, 5);
+            resp.getWriter().write("5 + 5 = " + addRes);
         }catch(Exception e){
             e.printStackTrace();
         }
